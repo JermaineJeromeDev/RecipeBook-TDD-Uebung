16d0b37e2b96e6ccc2bde257fea3afc2779a9651
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-03-24 13:44:24 +0100

    Add tests for unauthorized access and recipe creation with authentication

--- recipes_app/tests/test_api.py
+++ recipes_app/tests/test_api.py
@@ -2,27 +2,66 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from recipes_app.models import Recipe
 
 
-class RecipeAPITestCase(APITestCase):
-    def test_get_recipes_list(self):
-        """Aufgabe 2: GET /api/recipes-list/ liefert 200 OK"""
-        url = reverse('recipes-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+class RecipeAPITestCaseHappy(APITestCase):
+    """Hier testen wir alles, was funktionieren SOLL (mit Auth)."""
+    def setUp(self):
+        self.user = User.objects.create_user(username='happyuser', password='password123')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        # Wir erstellen vorab ein Rezept für die Detail-Tests
+        self.recipe = Recipe.objects.create(
+            title="Testsuppe", 
+            description="Lecker", 
+            author=self.user
+        )
 
-    def test_post_recipe_create(self):
-        """Aufgabe 3: POST /api/recipes-list/ erstellt ein Rezept"""
-        user = User.objects.create(username='testuser', password='password123')
-
+    def test_post_recipe_authenticated(self):
+        """Aufgabe 5: Eingeloggter User erstellt Rezept."""
         url = reverse('recipes-list')
         data = {
-            "title": "Mein Testrezept",
-            "description": "Sehr lecker",
-            "author": user.id 
+            "title": "Auth Rezept",
+            "description": "Mit Token erstellt",
+            "author": self.user.id
         }
-
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Recipe.objects.count(), 1)
+
+    def test_get_recipe_detail_authenticated(self):
+        """Aufgabe 6: Rezept-Detail abrufen mit Auth (200 OK)"""
+        # Hier nutzen wir kwargs für die ID des Rezepts
+        url = reverse('recipes-detail', kwargs={'pk': self.recipe.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], "Testsuppe")
+
+
+class RecipeAPITestCaseUnhappy(APITestCase):
+    """Hier testen wir Fehlerfälle (ohne Auth)."""
+    def setUp(self):
+        self.user = User.objects.create_user(username='otheruser')
+        self.recipe = Recipe.objects.create(
+            title="Geheimrezept", 
+            description="Nicht gucken", 
+            author=self.user
+        )
+
+    def test_post_recipe_unauthorized(self):
+        """Aufgabe 4: POST ohne Auth muss scheitern."""
+        url = reverse('recipes-list')
+        data = {"title": "Illegal", "description": "Kein Token", "author": 1}
+        response = self.client.post(url, data, format='json')
+
+        # Da wir oben IsAuthenticatedOrReadOnly gesetzt haben, 
+        # wird hier ein 401 (Unauthorized) erwartet.
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_recipe_detail_unauthorized(self):
+        """Aufgabe 8: Detailabruf ohne Auth gibt 401 (nach Aufgabe 7)"""
+        url = reverse('recipes-detail', kwargs={'pk': self.recipe.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
