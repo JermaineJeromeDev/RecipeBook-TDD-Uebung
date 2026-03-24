6e88f475ed8b028da9d5532e8039d0aa58013a29
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-03-24 13:32:32 +0100

    Add tests for recipe API: GET and POST methods

--- recipes_app/tests/test_api.py
+++ recipes_app/tests/test_api.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
+from recipes_app.models import Recipe
 
 
 class RecipeAPITestCase(APITestCase):
@@ -9,3 +11,18 @@ class RecipeAPITestCase(APITestCase):
         url = reverse('recipes-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_recipe_create(self):
+        """Aufgabe 3: POST /api/recipes-list/ erstellt ein Rezept"""
+        user = User.objects.create(username='testuser', password='password123')
+
+        url = reverse('recipes-list')
+        data = {
+            "title": "Mein Testrezept",
+            "description": "Sehr lecker",
+            "author": user.id 
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Recipe.objects.count(), 1)
